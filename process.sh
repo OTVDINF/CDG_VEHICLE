5300abba3d3c7c74b08dad54c00fc2ad28f1086e
Otavio <otavio@inf.ufpr.br>
2026-04-14 19:19:09 -0300

    Update process.sh

--- process.sh
+++ process.sh
@@ -1,24 +1,11 @@
-mkdir -p /home/otavio/LPLCv2_crops4
+mkdir -p /home/otavio/LPLCv2_crop2
 
-for dir in /home/otavio/images/*; do 
-    name=$(basename "$dir")
-    mkdir -p "/home/otavio/LPLCv2_crops4/$name"
+yolo predict model=yolov8m.pt device=0 \
+    source=/home/otavio/LPLCv2_completa/LPLCv2/images \
+    save_crop=True save=False classes=2,3,5,7 \
+    project=/home/otavio/tmp_yolo name=fast exist_ok=True
 
-    for img in "$dir"/*.{jpg,jpeg,png}; do 
-        [ -e "$img" ] || continue
-
-
-        yolo predict model=yolov8m.pt device=0 source="$img" save_crop=True save=False classes=2,3,5,7 project=/home/otavio/tmp_yolo name=temp exist_ok=True >/dev/null 2>&1
-
-        base=$(basename "$img")
-        base="${base%.*}"
-
-        i=1
-        for crop in /home/otavio/tmp_yolo/temp/crops/*/*; do
-            [ -f "$crop" ] || continue
-
-            new_name="${base}_${i}.jpg"
-            cp "$crop" "/home/otavio/LPLCv2_crops4/$name/$new_name"
+find /home/otavio/tmp_yolo/fast/crops -type f -exec cp {} /home/otavio/LPLCv2_crop2/ \;crops4/$name/$new_name"
 
             ((i++))
         done
